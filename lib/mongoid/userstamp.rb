a4baa8b194e83557db004d076a928e9764f9dc15
Thomas Boerger <tboerger@tbpro.de>
2013-03-24 13:53:24 +0100

    RReverted to old configuration behaviour

--- lib/mongoid/userstamp.rb
+++ lib/mongoid/userstamp.rb
@@ -9,41 +9,41 @@ module Mongoid
     autoload :User, 'mongoid/userstamp/user'
 
     included do
-      field self.userstamp_config.updated_column, :type => Object
-      field self.userstamp_config.created_column, :type => Object
+      field Mongoid::Userstamp.configuration.updated_column, :type => Object
+      field Mongoid::Userstamp.configuration.created_column, :type => Object
 
       before_save :set_updator
       before_create :set_creator
 
-      define_method self.userstamp_config.updated_accessor do
-        updated_col = self.send(self.userstamp_config.updated_column)
-        updated_col ? self.userstamp_config.user_model.find(updated_col) : nil
+      define_method Mongoid::Userstamp.configuration.updated_accessor do
+        updated_col = self.send(Mongoid::Userstamp.configuration.updated_column)
+        updated_col ? Mongoid::Userstamp.configuration.user_model.find(updated_col) : nil
       end
 
-      define_method self.userstamp_config.created_accessor do
-        created_col = self.send(self.userstamp_config.created_column)
-        created_col ? self.userstamp_config.user_model.find(created_col) : nil
+      define_method Mongoid::Userstamp.configuration.created_accessor do
+        created_col = self.send(Mongoid::Userstamp.configuration.created_column)
+        created_col ? Mongoid::Userstamp.configuration.user_model.find(created_col) : nil
       end
 
       protected
         def set_updator
-          return unless self.userstamp_config.user_model.respond_to? :current
-          column = "#{self.userstamp_config.updated_column.to_s}=".to_sym
+          return unless Mongoid::Userstamp.configuration.user_model.respond_to? :current
+          column = "#{Mongoid::Userstamp.configuration.updated_column.to_s}=".to_sym
 
-          self.send(column, self.userstamp_config.user_model.current.try(:id))
+          self.send(column, Mongoid::Userstamp.configuration.user_model.current.try(:id))
         end
 
         def set_creator
-          return unless self.userstamp_config.user_model.respond_to? :current
-          column = "#{self.userstamp_config.created_column.to_s}=".to_sym
+          return unless Mongoid::Userstamp.configuration.user_model.respond_to? :current
+          column = "#{Mongoid::Userstamp.configuration.created_column.to_s}=".to_sym
 
-          self.send(column, self.userstamp_config.user_model.current.try(:id))
+          self.send(column, Mongoid::Userstamp.configuration.user_model.current.try(:id))
         end
     end
 
-    module ClassMethods
-      def userstamp_config
-        @userstamp_config ||= Mongoid::Userstamp::Config.new
+    class << self
+      def configuration
+        @configuration ||= Mongoid::Userstamp::Config.new
       end
     end
   end
