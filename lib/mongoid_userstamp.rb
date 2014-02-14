fea3b8c99cf3c5fcd920f258bd7602e9b2134c4d
Bharat Gupta <bindassbharat311@gmail.com>
2014-02-14 12:51:33 +0530

    Added ability to specify various configurations for Userstamp models

--- lib/mongoid_userstamp.rb
+++ lib/mongoid_userstamp.rb
@@ -3,4 +3,5 @@ require 'mongoid/userstamp'
 require 'mongoid/userstamp/version'
 require 'mongoid/userstamp/config'
 require 'mongoid/userstamp/user'
+require 'mongoid/userstamp/userstampable'
 require 'mongoid/userstamp/railtie' if defined? Rails
