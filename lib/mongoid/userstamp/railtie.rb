17771de8867fdecf8b78ac10376942d97d897c61
Bharat Gupta <bindassbharat311@gmail.com>
2014-02-14 13:03:17 +0530

    Modified Railtie to allow for backwards compatibility, i.e. works w/o calling mongoid_userstamp in model

--- lib/mongoid/userstamp/railtie.rb
+++ lib/mongoid/userstamp/railtie.rb
@@ -4,15 +4,27 @@ module Mongoid
     class Railtie < Rails::Railtie
       ActiveSupport.on_load :action_controller do
         before_filter do |c|
-          unless Mongoid::Userstamp.config.user_model.respond_to? :current
-            Mongoid::Userstamp.config.user_model.send(
-              :include,
-              Mongoid::Userstamp::User
-            )
-          end
-
           begin
-            Mongoid::Userstamp.config.user_model.current = c.send(Mongoid::Userstamp.config.user_reader)
+
+            #### The below code is added for backwards compatibility so that the below works as well. In this case :default config is used.
+            #
+            # class Article
+            #   include Mongoid::Userstamp
+            # end
+            #
+            Mongoid::Userstamp.timestamped_models.each do |model|
+              unless model.included_modules.include?(Mongoid::Userstamp::Userstampable)
+                model.send(:include, Mongoid::Userstamp::Userstampable)
+              end
+            end if Mongoid::Userstamp.timestamped_models
+
+            Mongoid::Userstamp.configs.each_pair do |key, config|
+              unless config.user_model.respond_to?(:current)
+                config.user_model.send(:include, Mongoid::Userstamp::User)
+              end
+
+              config.user_model.current = c.send(config.user_reader)
+            end
           rescue
           end
         end
