455d312e5a4fb2d26785c6f9e948ee990e4ab182
Johnny Shields <johnny.shields@gmail.com>
2014-02-24 17:13:22 +0900

    Second attempt at better Railtie

--- lib/mongoid/userstamp/railtie.rb
+++ lib/mongoid/userstamp/railtie.rb
@@ -6,7 +6,7 @@ module Userstamp
   class Railtie < Rails::Railtie
 
     # Include Mongoid::Userstamp::User into User class, if not already done
-    initializer 'mongoid_userstamp.setup_user_classes' do |app|
+    config.to_prepare do
       Mongoid::Userstamp.user_classes.each do |user_class|
         unless user_class.included_modules.include?(Mongoid::Userstamp::User)
           user_class.send(:include, Mongoid::Userstamp::User)
@@ -16,7 +16,7 @@ module Userstamp
 
     # Add userstamp to models where Mongoid::Userstamp was included, but
     # mongoid_userstamp was not explicitly called
-    initializer 'mongoid_userstamp.setup_model_classes' do |app|
+    config.to_prepare do
       Mongoid::Userstamp.model_classes.each do |model_class|
         unless model_class.included_modules.include?(Mongoid::Userstamp::Model)
           model_class.send(:include, Mongoid::Userstamp::Model)
@@ -25,14 +25,12 @@ module Userstamp
     end
 
     # Set current_user from controller reader method
-    initializer 'mongoid_userstamp.setup_current_user_before_filter' do |app|
-      ActiveSupport.on_load :action_controller do
-        before_filter do |c|
-          Mongoid::Userstamp.user_classes.each do |user_class|
-            begin
-              user_class.current = c.send(user_class.mongoid_userstamp_user.reader)
-            rescue
-            end
+    ActiveSupport.on_load :action_controller do
+      before_filter do |c|
+        Mongoid::Userstamp.user_classes.each do |user_class|
+          begin
+            user_class.current = c.send(user_class.mongoid_userstamp_user.reader)
+          rescue
           end
         end
       end
